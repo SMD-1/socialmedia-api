8d8f9c90001a5cd72d8126c56f5e2ce1dc892c21
Danish <skdanish1999@gmail.com>
2022-01-17 23:54:23 +0530

    send and get messeges

--- socket/index.js
+++ socket/index.js
@@ -14,11 +14,24 @@ const addUser = (userId, socketId) => {
 const removeUser = (socketId) => {
   users = users.filter((user) => user.socketId !== socketId);
 };
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 io.on("connection", (socket) => {
-  console.log("user connected");
+  console.log("user connected", socket.id);
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
     io.emit("getUsers", users);
+  });
+
+  // send and get messages
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
   });
 
   socket.on("disconnect", () => {
